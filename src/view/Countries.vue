b3e31d01b9ac65cf976f71325207d44647ffa9b1
DDobos <danandreidobos@gmail.com>
2021-01-17 17:02:25 +0200

    added card, responsive image, v-if for population

--- src/view/Countries.vue
+++ src/view/Countries.vue
@@ -1,12 +1,67 @@
 <template>
   <div class="container is-fluid">
-    <div class="columns is-multiline">
-      <div
-        class="column is-one-third-desktop is-half-tablet is-one-quarter-fullhd"
-        v-for="item in countries"
-        :key="item.name"
-      >
-        {{ item.name }}
+    <div class="container is-max-fullhd">
+      <div class="columns is-multiline">
+        <div
+          class="column is-full-mobile is-half-tablet is-one-third-desktop is-one-quarter-fullhd"
+          v-for="(country, index) in countries"
+          :key="index"
+        >
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-3by2 m-5">
+                <img :src="country.flag" alt="flag" />
+              </figure>
+            </div>
+
+            <div class="card-content has-text-justified">
+              <div class="columns is-mobile">
+                <h4 class="column is-4 is-half-mobile">Country:</h4>
+                <div class="column has-text-right-mobile">
+                  {{ country.name }}
+                </div>
+              </div>
+
+              <div class="columns is-mobile">
+                <div class="column is-4">Capital:</div>
+                <div class="column has-text-right-mobile">
+                  {{ country.capital }}
+                </div>
+              </div>
+
+              <div class="columns is-mobile">
+                <div class="column is-4">Region:</div>
+                <div class="column has-text-right-mobile">
+                  {{ country.region }}
+                </div>
+              </div>
+
+              <div class="columns is-multiline is-mobile">
+                <div class="column is-4">
+                  <p>Timezones:</p>
+                </div>
+                <div class="column is-8 has-text-right-mobile">
+                  <div
+                    v-for="(item, i) in country.timezones"
+                    :key="`item-${i}`"
+                  >
+                    {{ item }}
+                  </div>
+                </div>
+              </div>
+
+              <div class="columns is-mobile">
+                <div class="column is-4">Population:</div>
+                <div class="column has-text-right-mobile">
+                  <div v-if="country.population > 1000">
+                    {{ Math.round(country.population / 1000) }}K
+                  </div>
+                  <div v-else>{{ country.population }}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -15,7 +70,6 @@
 <script>
 export default {
   name: "Countries",
-
   computed: {
     getcountries() {
       return this.$store.getters.allCountries;
